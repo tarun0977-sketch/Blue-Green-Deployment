471e5bfd072ba4bfb8345ab956fb7e3322424e4d
veeranjaneyulu <107455720+nani03007@users.noreply.github.com>
2026-04-12 17:22:22 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -111,7 +111,7 @@ resource "aws_eks_node_group" "devopsshack" {
   instance_types = ["t2.large"]
 
   remote_access {
-    ec2_ssh_key = var.ssh_key_name
+    ec2_ssh_key = "bluegreen"
     source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
   }
 }
